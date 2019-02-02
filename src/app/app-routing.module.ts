c35584b4dd98c959745b3671ac10221eb0734136
shrishtiraghav <shrishtiraghav1998@gmail.com>
2019-02-02 21:09:24 +0530

    secondcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
   },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
+  
   { path: 'shop', loadChildren: './shop/shop.module#ShopPageModule' },
   { path: 'retailer', loadChildren: './retailer/retailer.module#RetailerPageModule' },
  
